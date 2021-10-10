f593ad2a910cf6e22411d08d9799c2806161842b
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2021-10-10 18:49:37 +0200

    renamed HelloControlClass

---
+++ src/main/java/com/herokuapi001/Controllers/HelloControlClass.java
@@ -0,0 +1,17 @@
+package com.herokuapi001.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HelloControlClass {
+
+    @GetMapping("/first") @ResponseBody public String first() {
+        return "hello world, yall motherfuckers";
+    }
+
+
+
+}
